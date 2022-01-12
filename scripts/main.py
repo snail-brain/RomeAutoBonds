4e6579278c791962f878e8f715e95d91f39cfe1a
snail-brain <snail-brain@protonmail.com>
2022-01-12 15:30:51 -0600

    back to mac

--- scripts/main.py
+++ scripts/main.py
@@ -15,13 +15,13 @@ staking_rate = to_distribute / total_staked
 def real_deal():
     best_deal = check_discounts()
     print("Best deal: ", best_deal)
-    total_rome = rome.balanceOf(account.address) + \
-        sRome.balanceOf(account.address)
+    total_rome = rome.balanceOf(account.address) + sRome.balanceOf(account.address)
 
     # Compare end-of-bond rome amount to default stake amount
     ending_rome_no_bond = rome_staked(total_rome, staking_rate)
     ending_rome_yes_bond = rome_bond_stake(
-        total_rome + (total_rome * best_deal[0]), staking_rate)
+        total_rome + (total_rome * best_deal[0]), staking_rate
+    )
 
     print("No bond: ", ending_rome_no_bond, "Yes bond: ", ending_rome_yes_bond)
 
@@ -31,13 +31,17 @@ def real_deal():
         if best_deal[1] == romeFrax:
             add_rome_frax_liq(rome.balance(), {"from": account})
         else:
-            make_swap(rome.balanceOf(account.address),
-                      best_deal[3], best_deal[1], {'from': account})
+            make_swap(
+                rome.balanceOf(account.address),
+                best_deal[3],
+                best_deal[1],
+                {"from": account},
+            )
             bond(best_deal[1], best_deal[2], {"from": account})
 
 
 def if_end_is_near():
-    if(next_action_block < len(chain) - 1):
+    if next_action_block < len(chain) - 1:
         redeem_bonds()
         stake(rome.balanceOf(account.address))
         new_block = next_action_block + epoch_length
